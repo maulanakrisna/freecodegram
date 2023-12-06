ee03473548cf65daec322415bccd843bcd057e57
maulanakrisna <51012366+maulanakrisna@users.noreply.github.com>
2023-12-06 09:05:40 +0800

    first commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,13 @@
+@component('mail::message')
+# Welcome to FreeCodeGram
+
+This is community of fellow developers and we love that you have joined us.
+
+{{-- @component('mail::button', ['url' => ''])
+Button Text
+@endcomponent --}}
+
+All the best,<br>
+{{-- {{ config('app.name') }} --}}
+Maulana
+@endcomponent
